9f1cc1551a13a1c87d5c6447c5adcb25561f5686
Heejun Byeon <hejuby@gmail.com>
2024-08-14 01:27:05 +0900

    feat: 공통 Button 컴포넌트 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,35 @@
+import { createElement, ButtonHTMLAttributes } from "react";
+import ms from "@/utils/modifierSelector";
 import styles from "./index.module.scss";
 
-const Button = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <button type="button" className={styles["button--active"]}>
-      {children}
-    </button>
+type ButtonProps = {
+  color?: "solid" | "outline";
+  size?: "small" | "medium" | "large";
+  full?: boolean;
+} & {
+  children: React.ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const cn = ms(styles, "button");
+
+const Button = ({
+  children,
+  color = "solid",
+  size = "small",
+  full = false,
+  type = "button",
+  disabled = false,
+  ...props
+}: ButtonProps) =>
+  createElement(
+    "button",
+    {
+      className: cn(`--color-${color}`, `--size-${size}`, full ? "--full" : ""),
+      type: type === "submit" ? "submit" : "button",
+      disabled,
+      ...props,
+    },
+    children,
   );
-};
 
 export default Button;
