09d16da6fa2708b647c17b499b0657bf5a7a18be
santa-developer <finalatro37@naver.com>
2024-08-13 16:42:37 +0900

    테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <main>메인입니다</main>;
+  return (
+    <main>
+      메인입니다
+      <label>
+        <input></input>
+      </label>
+    </main>
+  );
 };
 
 export default Home;
