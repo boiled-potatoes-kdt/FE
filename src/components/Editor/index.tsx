20a894576ab69dd7167355c828df3dae821927ce
Heejun Byeon <hejuby@gmail.com>
2024-08-21 18:29:44 +0900

    feat: 기본적인 React Quill 에디터 생성

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+<<<<<<< HEAD
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
@@ -32,6 +33,65 @@ const Editor = () => {
       <Toolbar editor={editor} />
       <EditorContent editor={editor} />
     </>
+=======
+// import { useRef } from "react";
+// import Quill from "quill";
+// import EditorContent from "./EditorContent";
+
+// const Delta = Quill.import("delta");
+
+// const Editor = () => {
+//   const quillRef = useRef<HTMLDivElement>(null);
+
+//   return <EditorContent defaultValue={new Delta()} ref={quillRef} />;
+// };
+
+// export default Editor;
+
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+
+const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
+
+const Editor = () => {
+  const [content, setContent] = useState("");
+
+  const quillModules = {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      [{ color: [] }],
+      ["bold", "italic", "underline", "strike"],
+      ["image"],
+      [{ align: [] }],
+    ],
+  };
+
+  const quillFormats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "list",
+    "bullet",
+    "image",
+    "align",
+    "color",
+  ];
+
+  const handleEditorChange = (newContent: string) => {
+    setContent(newContent);
+  };
+
+  return (
+    <QuillEditor
+      value={content}
+      onChange={handleEditorChange}
+      modules={quillModules}
+      formats={quillFormats}
+    />
+>>>>>>> 1674048 (feat: 기본적인 React Quill 에디터 생성)
   );
 };
 
