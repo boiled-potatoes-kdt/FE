2c2db11a0ca92e48e1fe5fe42a7a11e6f8a589d6
Heejun Byeon <hejuby@gmail.com>
2024-08-22 13:39:57 +0900

    feat: Paginatino 컴포넌트 추가

--- src/components/Pagination/PaginationButton/index.tsx
+++ src/components/Pagination/PaginationButton/index.tsx
@@ -3,6 +3,7 @@ import styles from "./index.module.scss";
 
 interface PaginationButtonProps {
   children: React.ReactNode;
+  label: string;
   isActive?: boolean;
 }
 
@@ -10,10 +11,11 @@ const span = ms(styles, "span");
 
 const PaginationButton = ({
   children,
+  label,
   isActive = false,
 }: PaginationButtonProps) => {
   return (
-    <button type="button" className={styles.button}>
+    <button type="button" className={styles.button} aria-label={label}>
       {typeof children === "string" || typeof children === "number" ? (
         <span className={span(isActive && "--active")}>{children}</span>
       ) : (
