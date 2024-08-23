86e04e9fa357d9b14699103110a2835cef7afcee
Heejun Byeon <hejuby@gmail.com>
2024-08-23 18:16:30 +0900

    refactor: 게시판 상수 네이밍 통일

--- src/components/Board/ListItem/index.tsx
+++ src/components/Board/ListItem/index.tsx
@@ -3,7 +3,7 @@ import formatDate from "@/utils/formatDate";
 import ms from "@/utils/modifierSelector";
 import IconProfile from "@/assets/icons/icon-profile.svg";
 import IconComment from "@/assets/icons/icon-comment.svg";
-import { BoardType, CATEGORY_LIST, CategoryId } from "@/@types/board";
+import { BoardType, CategoryId, CATEGORY_LIST } from "@/@types/board";
 import styles from "./index.module.scss";
 
 export interface CommunityItemProps {
@@ -36,13 +36,13 @@ const ListItem = ({
   const category = CATEGORY_LIST[boardType].find(
     (categoryItem) => categoryItem.categoryId === categoryId,
   );
-  const { categoryName, categoryText } =
+  const { categoryType, categoryName } =
     category || CATEGORY_LIST[boardType][0];
 
   return (
     <article className={styles.article}>
-      <p className={ac(`--board-${boardType}--category-${categoryName}`)}>
-        {categoryText}
+      <p className={ac(`--board-${boardType}--category-${categoryType}`)}>
+        {categoryName}
       </p>
       <Link href={`/board/community/${id}`} className={styles.article__link}>
         <h3 className={styles.article__title}>{title}</h3>
