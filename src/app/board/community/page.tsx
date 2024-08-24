1b393be1478bd2e7965a72b3d348ef08053b5888
Heejun Byeon <hejuby@gmail.com>
2024-08-24 14:42:49 +0900

    feat: 데스크탑 게시판 글쓰기 버튼 추가

--- src/app/board/community/page.tsx
+++ src/app/board/community/page.tsx
@@ -1,7 +1,8 @@
 import SideNav from "@/components/Board/SideNav";
 import Title from "@/components/Board/Title";
-import CategoryTab from "@/components/CategoryTab";
 import Search from "@/components/Board/Search";
+import CategoryTab from "@/components/CategoryTab";
+import PostButton from "@/components/Board/PostButton";
 import ListItem, { CommunityItemProps } from "@/components/Board/ListItem";
 import Pagination from "@/components/Pagination";
 import styles from "./page.module.scss";
@@ -39,6 +40,7 @@ const Community = () => {
             <Search />
             <CategoryTab tabs={tabs} />
           </nav>
+          <PostButton />
         </section>
         <ul>
           {BOARD_LIST.map((boardItem) => (
