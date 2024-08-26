66c17aa4724cfd13e75f005a497a71b3ea6a9339
santa-developer <finalatro37@naver.com>
2024-08-26 22:21:18 +0900

    feat: 현재 페이지 경로에 따른 헤더 타이틀 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,37 +1,44 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import useHeaderStore from "@/store/useHeaderStore";
+import titles from "@/data/header_title.json";
 import Logo from "@/assets/icons/logo.svg";
 import MobileLogo from "@/assets/icons/logo-mobile.svg";
 import IconSearch from "@/assets/icons/icon-search.svg";
 import IconHamberger from "@/assets/icons/icon-hamburger.svg";
 import IconClose from "@/assets/icons/icon-close.svg";
+import IconLeft from "@/assets/icons/icon-direction-left.svg";
 import Link from "next/link";
 import styles from "./index.module.scss";
 import UserInfo from "./UserInfo";
 import MobileMenu from "./MobileMenu";
 
 const Header = () => {
-  const [isTablet, setIsTablet] = useState(false);
+  const [isTablet, setIsTablet] = useState(window.innerWidth <= 1024);
   const [showMenu, setShowMenu] = useState(false);
 
-  const handleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+  const router = useRouter();
+  const pathname = usePathname();
+  const setTitle = useHeaderStore((state) => state.setTitle);
+  const title = useHeaderStore((state) => state.title);
+
+  const toggleMenu = () => setShowMenu((prev) => !prev);
 
   useEffect(() => {
-    const screenWidth = () => {
-      if (window.innerWidth <= 1024) {
-        setIsTablet(true);
-      } else {
-        setIsTablet(false);
-      }
-    };
-    screenWidth();
-
-    window.addEventListener("resize", screenWidth);
-    return () => window.removeEventListener("resize", screenWidth);
+    const handleResize = () => setIsTablet(window.innerWidth <= 1024);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    setTitle(titles[pathname as keyof typeof titles] || "다인 리뷰");
+  }, [pathname, setTitle]);
+
+  const isAccountPage = pathname.startsWith("/account");
+  const isHomePage = pathname === "/";
+
   return (
     <>
       <header className={styles.header}>
@@ -44,7 +51,7 @@ const Header = () => {
                 </Link>
                 <Link href="/">체험단</Link>
                 <Link href="/">이용안내</Link>
-                <Link href="/">게시판</Link>
+                <Link href="/announcement">게시판</Link>
               </div>
               <div className={styles["header__content--right"]}>
                 <UserInfo />
@@ -53,8 +60,22 @@ const Header = () => {
           ) : (
             <>
               <div className={styles["header__mobile-left"]}>
-                <MobileLogo />
+                {!isHomePage ? (
+                  <button onClick={() => router.back()}>
+                    <IconLeft />
+                  </button>
+                ) : (
+                  <Link href="/">
+                    <MobileLogo />
+                  </Link>
+                )}
               </div>
+              {isAccountPage ? (
+                <MobileLogo />
+              ) : isHomePage ? null : (
+                <p>{title}</p>
+              )}
+
               <ul className={styles["header__mobile-right"]}>
                 <li>
                   <Link href="/">
@@ -65,7 +86,7 @@ const Header = () => {
                   <button
                     type="button"
                     aria-label="hamberger-button"
-                    onClick={handleMenu}
+                    onClick={toggleMenu}
                   >
                     {showMenu ? <IconClose /> : <IconHamberger />}
                   </button>
@@ -75,7 +96,7 @@ const Header = () => {
           )}
         </div>
       </header>
-      {showMenu && <MobileMenu />}
+      {isTablet && showMenu && <MobileMenu setShowMenu={setShowMenu} />}
     </>
   );
 };
