49f9b5e3f97dd79b5d56bce784bbefa55350ef77
minnug <parkminnug@gmail.com>
2024-08-26 22:40:46 +0900

    feat: 아이디 찾기 완료 생성

---
+++ src/app/account/id-find-success/page.tsx
@@ -0,0 +1,6 @@
+import IdFindSuccess from "@/components/Account/IdFindSuccess";
+
+const Login = () => {
+  return <IdFindSuccess />;
+};
+export default Login;
---
+++ src/components/Account/IdFindSuccess/index.tsx
@@ -0,0 +1,28 @@
+import IconCheck from "@/assets/icons/icon-check-id-find.svg";
+import Button from "@/components/Button";
+import styles from "./index.module.scss";
+
+const IdFindSuccess = () => {
+  return (
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <IconCheck />
+        <h2>아이디 찾기 완료</h2>
+        <p>입력하신 정보와 일치하는 아이디는 아래와 같습니다.</p>
+      </header>
+      <div className={styles["result-wrapper"]}>
+        <p>gam**08@gmail.com</p>
+      </div>
+      <div className={styles["button-container"]}>
+        <Button size="medium" full>
+          로그인
+        </Button>
+        <Button size="medium" color="outline" full>
+          비밀번호 찾기
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default IdFindSuccess;
