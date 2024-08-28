c735beaa8ac24820b31d26af55c02e820e1178de
Heejun Byeon <hejuby@gmail.com>
2024-08-29 00:02:53 +0900

    feat: 체험단 관리 ManageProgress 컴포넌트 추가

--- src/components/Campaign/ManageProgress/index.tsx
+++ src/components/Campaign/ManageProgress/index.tsx
@@ -1,5 +1,34 @@
-const ManageProgress = () => {
-  return <article></article>;
+import IconRecruit from "@/assets/icons/icon-recruit.svg";
+import IconRecruitFinish from "@/assets/icons/icon-recruit-finish.svg";
+import IconReview from "@/assets/icons/icon-review.svg";
+import IconReviewFinish from "@/assets/icons/icon-review-finish.svg";
+import IconDirectionRight from "@/assets/icons/icon-direction-right.svg";
+
+const PROGRESS_STEPS: { icon: () => React.ReactNode; text: string }[] = [
+  { icon: () => <IconRecruit />, text: "모집 중" },
+  { icon: () => <IconRecruitFinish />, text: "모집종료" },
+  { icon: () => <IconReview />, text: "체험&리뷰" },
+  { icon: () => <IconReviewFinish />, text: "리뷰마감" },
+];
+
+interface ManageProgressProps {
+  activeIndex: number;
+}
+
+const ManageProgress = ({ activeIndex }: ManageProgressProps) => {
+  return (
+    <article>
+      {PROGRESS_STEPS.map((step, index) => (
+        <>
+          <figure>
+            {step.icon()}
+            <figcaption>{step.text}</figcaption>
+          </figure>
+          {index !== PROGRESS_STEPS.length && <IconDirectionRight />}
+        </>
+      ))}
+    </article>
+  );
 };
 
 export default ManageProgress;
