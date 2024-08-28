62b0d855ca6b8b06a2af63c986a2463a8da90c89
Heejun Byeon <hejuby@gmail.com>
2024-08-29 02:05:07 +0900

    feat: 체험단 관리 ManageProgress 컴포넌트 조정 및 스타일 완성

--- src/app/campaign/[id]/manage/page.tsx
+++ src/app/campaign/[id]/manage/page.tsx
@@ -1,5 +1,12 @@
+import ManageProgress from "@/components/Campaign/ManageProgress";
+
 const Page = () => {
-  return <h2>체험단 관리</h2>;
+  return (
+    <h2>
+      체험단 관리
+      <ManageProgress activeIndex={0} />
+    </h2>
+  );
 };
 
 export default Page;
