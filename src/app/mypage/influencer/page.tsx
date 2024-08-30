270a509452266ea40800983a0220b33020b775e4
minnug <parkminnug@gmail.com>
2024-08-30 17:03:19 +0900

    fix: 인플루언서 마이페이지 수정

--- src/app/mypage/influencer/page.tsx
+++ src/app/mypage/influencer/page.tsx
@@ -5,9 +5,9 @@ import Selectbox, { Option } from "@/components/Selectbox/index";
 import CampaignItemInfluencer from "@/components/Mypage/Influencer/CampaignItem";
 import CountBox from "@/components/Mypage/Influencer/CountBox";
 import ProfileBoxInfluencer from "@/components/Mypage/Influencer/ProfileBox";
-import InteractionList from "@/components/Mypage/InteractionList";
 import Searchbox from "@/components/Mypage/Searchbox";
-import CampaignEmpty from "@/components/Mypage/CampaignEmpty";
+// import CampaignEmpty from "@/components/Mypage/CampaignEmpty";
+import InteractionListInfluencer from "@/components/Mypage/Influencer/InteractionList";
 import styles from "./page.module.scss";
 
 const campaignItems = [
@@ -54,11 +54,10 @@ const MypageInfluencerPage = () => {
       <h2 className="visually-hidden">마이페이지</h2>
       <section className={styles.layout__left}>
         <ProfileBoxInfluencer />
-        <InteractionList />
+        <InteractionListInfluencer />
       </section>
       <div className={styles.layout__right}>
         <section>
-          <h3 className={styles["sub-title"]}>나의 활동</h3>
           <CountBox
             countItems={[
               { title: "신청한 체험단", count: 0 },
@@ -70,36 +69,40 @@ const MypageInfluencerPage = () => {
         </section>
         <section>
           <h3 className={styles["sub-title"]}>체험단</h3>
-          <div className={styles["search-container"]}>
-            <Selectbox
-              placeholder="플랫폼"
-              size="medium"
-              selected={selectedItem1}
-              options={[
-                { optionLabel: "인스타", value: "instargram" },
-                { optionLabel: "블로그", value: "blog" },
-                { optionLabel: "틱톡", value: "tictock" },
-                { optionLabel: "유튜브", value: "youtube" },
-                { optionLabel: "기타", value: "etc" },
-              ]}
-              onChange={setSelectedItem1}
-            />
-            <Selectbox
-              placeholder="상태"
-              size="medium"
-              selected={selectedItem2}
-              options={[
-                { optionLabel: "모집중", value: "recruitment" },
-                { optionLabel: "모집완료", value: "complete" },
-                { optionLabel: "체험&리뷰", value: "review" },
-                { optionLabel: "리뷰마감", value: "deadline" },
-              ]}
-              onChange={setSelectedItem2}
-            />
+          <div className={styles["campaign-search"]}>
+            <div className={styles.search__select}>
+              <Selectbox
+                placeholder="플랫폼"
+                size="medium"
+                selected={selectedItem1}
+                options={[
+                  { optionLabel: "인스타", value: "instargram" },
+                  { optionLabel: "블로그", value: "blog" },
+                  { optionLabel: "틱톡", value: "tictock" },
+                  { optionLabel: "유튜브", value: "youtube" },
+                  { optionLabel: "기타", value: "etc" },
+                ]}
+                onChange={setSelectedItem1}
+              />
+              <Selectbox
+                placeholder="상태"
+                size="medium"
+                selected={selectedItem2}
+                options={[
+                  { optionLabel: "모집중", value: "recruitment" },
+                  { optionLabel: "모집완료", value: "complete" },
+                  { optionLabel: "체험&리뷰", value: "review" },
+                  { optionLabel: "리뷰마감", value: "deadline" },
+                ]}
+                onChange={setSelectedItem2}
+              />
+            </div>
             <Searchbox />
           </div>
-          <CampaignItemInfluencer campaignItems={campaignItems} />
-          <CampaignEmpty />
+          <div className={styles.campaign__list}>
+            <CampaignItemInfluencer campaignItems={campaignItems} />
+            {/* <CampaignEmpty /> */}
+          </div>
         </section>
       </div>
     </div>
