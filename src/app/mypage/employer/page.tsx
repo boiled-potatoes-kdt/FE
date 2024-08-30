1ec6e6da52b750d441d8a9c84e0ce7642803fb80
minnug <parkminnug@gmail.com>
2024-08-30 17:57:07 +0900

    feat: 마이페이지 모바일 구분선 추가

--- src/app/mypage/employer/page.tsx
+++ src/app/mypage/employer/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Selectbox, { Option } from "@/components/Selectbox/index";
 import Searchbox from "@/components/Mypage/Searchbox";
 import ProfileBoxEmployer from "@/components/Mypage/Employer/ProfileBox";
@@ -48,6 +48,19 @@ const MypageEmployerPage = () => {
   const [selectedItem1, setSelectedItem1] = useState<Option | null>(null);
   const [selectedItem2, setSelectedItem2] = useState<Option | null>(null);
 
+  const [isTablet, setIsTablet] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTablet(window.innerWidth <= 1024);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={styles.layout}>
       <h2 className="visually-hidden">마이페이지</h2>
@@ -55,6 +68,7 @@ const MypageEmployerPage = () => {
         <ProfileBoxEmployer />
         <InteractionListEmployer />
       </section>
+      {isTablet && <div className={styles.divider} />}
       <div className={styles.layout__right}>
         <section>
           <h3 className={styles["sub-title"]}>체험단</h3>
