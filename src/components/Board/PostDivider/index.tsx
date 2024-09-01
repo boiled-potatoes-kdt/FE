e9f8fb0fa7cc3b065a4aca232c6af8641c3e4e08
Heejun Byeon <hejuby@gmail.com>
2024-09-02 00:04:32 +0900

    feat: 게시판 PostDivider 추가

---
+++ src/components/Board/PostDivider/index.tsx
@@ -0,0 +1,12 @@
+import Line from "@/components/Line";
+import styles from "./index.module.scss";
+
+const PostDivider = () => {
+  return (
+    <section className={styles.divider}>
+      <Line />
+    </section>
+  );
+};
+
+export default PostDivider;
