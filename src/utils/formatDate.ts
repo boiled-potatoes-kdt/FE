50e9db2db33e60cd0294fe8325704a39cba872f2
Heejun Byeon <hejuby@gmail.com>
2024-09-15 12:03:45 +0900

    fix: formatDate 함수 parseInt 인자 에러 수정

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,8 @@
+const WEEKDAYS = ["일", "월", "화", "수", "목", "금", "토"] as const;
+
 const formatDate = (
   dateString: string | undefined,
-  format: "YMD" | "MDMH" | "AbbrYMD",
+  format: "YMD" | "MDMH" | "AbbrYMD" | "YMDDay",
 ) => {
   if (!dateString) {
     return null;
@@ -14,9 +16,7 @@ const formatDate = (
       return acc;
     }, []);
   const shortYear = year.slice(2);
-  const date = new Date(`${year}-${month}-${day}`);
-  const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
-  const dayOfWeek = weekdays[date.getDay()];
+  const dayOfWeek = WEEKDAYS[new Date(dateString).getDay()];
 
   if (format === "YMD") {
     return `${year}년 ${month}월 ${day}월`;
@@ -25,10 +25,10 @@ const formatDate = (
     return `${month}/${day} ${hour}:${minute}`;
   }
   if (format === "AbbrYMD") {
-    return `${shortYear}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일`;
+    return `${shortYear}년 ${month}월 ${day}일`;
   }
   if (format === "YMDDay") {
-    return `${shortYear}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일 (${dayOfWeek})`;
+    return `${shortYear}년 ${month}월 ${day}일 (${dayOfWeek})`;
   }
   return null;
 };
